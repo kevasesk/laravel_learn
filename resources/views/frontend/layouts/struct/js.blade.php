57a4d3b9263cd65f1499dbc173b87515b59fc315
kevasesk <kevasqwerty@gmail.com>
2023-02-07 19:53:11 +0200

    fix js static. remove vendor rar. fix sessions?

--- resources/views/frontend/layouts/struct/js.blade.php
+++ resources/views/frontend/layouts/struct/js.blade.php
@@ -1,5 +1,5 @@
-<script type="text/javascript" src="/js/bootstrap.js"></script>
-<script type="text/javascript" src="/js/owl.carousel.min.js"></script>
-<script type="text/javascript" src="/js/slick.min.js"></script>
-<script type="text/javascript" src="/js/main.js"></script>
-<script type="text/javascript" src="/js/bootstrap-slider.min.js"></script>
+<script type="text/javascript" src="{{asset('/js/bootstrap.js')}}"></script>
+<script type="text/javascript" src="{{asset('/js/owl.carousel.min.js')}}"></script>
+<script type="text/javascript" src="{{asset('/js/slick.min.js')}}"></script>
+<script type="text/javascript" src="{{asset('/js/main.js')}}"></script>
+<script type="text/javascript" src="{{asset('/js/bootstrap-slider.min.js')}}"></script>
